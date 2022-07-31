71f59f917fc7b2f94e23c9d3a4d13c281c1b2534
Dylan Tettarasar <dtettarasar@gmail.com>
2022-07-31 17:06:34 +0200

    feu01 : created checkParenthesis func + update errormessage

--- feu01.js
+++ feu01.js
@@ -3,26 +3,27 @@
 const argTester = () => {
 
     const argument = process.argv.slice(2);
-    const errMsg = "erreur argument";
+    const errMsg = "erreur argument: ";
     const intPattern = /^(\+?|-?)[0-9]+$/;
     const sepPattern = /\S/m;
     const expArr = [];
 
     if (argument.length !== 1) {
 
-        console.log(errMsg);
+        console.log(errMsg + "il faut passer une seule chaîne de caractère en argument.");
         return false;
-        
+
     }
 
     const strSplit = argument[0].split('');
 
+    // Vérifier que l'expression ne comporte que des opérateurs et des nombres
     for (let i = 0; i < strSplit.length; i++) {
         
         const testChar = intPattern.test(strSplit[i]) || findOperators(strSplit[i]);
         const msgToLog = "char: " + strSplit[i] + " : " + testChar;
 
-        console.log(msgToLog);
+        // console.log(msgToLog);
 
         if (testChar) {
 
@@ -30,14 +31,49 @@ const argTester = () => {
 
         } else if (sepPattern.test(strSplit[i])) {
 
-            console.log(errMsg);
+            console.log(errMsg + "l'expression ne doit comporter que des opérateurs et des nombres.");
             return false;
 
         }
 
     }
 
+    const testParenthesis = checkParenthesis(expArr);
+
+    if (!testParenthesis) {
+
+        console.log(errMsg + "vérifier les parenthèses.");
+        return false;
+
+    }
+
     console.log(expArr.join(''));
+
+}
+
+const checkParenthesis = (array) => {
+
+    const openParenthesis = "(";
+    const closeParenthesis = ")";
+
+    let amountOpPar = 0;
+    let amountClPar = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        
+        if (array[i] === openParenthesis) {
+            amountOpPar++;
+        } else if (array[i] === closeParenthesis) {
+            amountClPar++;
+        }
+
+    }
+
+    if (amountClPar !== amountOpPar) {
+        return false;
+    } else {
+        return true;
+    }
 
 }
 
