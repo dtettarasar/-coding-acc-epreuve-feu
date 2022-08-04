bd32525a1dabd5f4c290eae592872ff812f82f17
Dylan Tettarasar <dtettarasar@gmail.com>
2022-08-04 12:01:50 -0400

    feu01 : update in argTester

--- feu01.js
+++ feu01.js
@@ -17,39 +17,98 @@ const argTester = () => {
 
     const strSplit = argument[0].split('');
 
+    let intArr = [];
+
     // Vérifier que l'expression ne comporte que des opérateurs et des nombres
     for (let i = 0; i < strSplit.length; i++) {
 
-        const expSpecChars = ["+","-","*","/","%","(",")"];
+        const expSpecChars = ["+","-","*","/","%"];
+        const parChar = ["(",")"];
         
         const testChar = intPattern.test(strSplit[i]) || findChars(strSplit[i], expSpecChars);
+
+        const charIsInt = intPattern.test(strSplit[i]);
+        const charIsOp = findChars(strSplit[i], expSpecChars);
+        const charIsPar = findChars(strSplit[i], parChar);
+
         const msgToLog = "char: " + strSplit[i] + " : " + testChar;
+
+        if (charIsInt) {
+            intArr.push(strSplit[i]);
+        } else if (charIsOp && intArr.length !== 0) {
+            expArr.push(intArr);
+            intArr = [];
+            expArr.push(strSplit[i]);
+        } else if (charIsPar && intArr.length == 0) {
+            expArr.push(strSplit[i]);
+        }
+
+        if (i === strSplit.length - 1) {
+
+            if (intArr.length !== 0) {
+                expArr.push(intArr);
+                intArr = [];
+            } else if (charIsPar) {
+                expArr.push(strSplit[i]);
+            }
+
+        }
+
+        /*
+        if (i === strSplit.length - 1 && intArr.length !== 0) {
+            expArr.push(intArr);
+            intArr = [];
+        } else if (i === strSplit.length - 1 && charIsPar) {
+            console.log("end");
+            expArr.push(strSplit[i]);
+        }*/
+
+
+
+        /*
+        console.log("intArr: " + intArr);
+
+        console.log("i value " + i);
+
+        console.log(strSplit[i]);
+        console.log("int: " + charIsInt);
+        console.log("spec: " + charIsSpec);
+        console.log("-------");
+        */
 
         // console.log(msgToLog);
 
+        /*
         if (testChar) {
 
-            expArr.push(strSplit[i]);
+            if (intPattern.test(strSplit[i])) {
+                intArr.push(strSplit);
+            } else if (intArr.length !== 0) {
+                expArr.push(intArr);
+                expArr.push(strSplit[i]);
+                intArr = [];
+            }
 
         } else if (sepPattern.test(strSplit[i])) {
 
             console.log(errMsg + "l'expression ne doit comporter que des opérateurs et des nombres.");
             return false;
 
-        }
+        }*/
 
     }
 
     const testParenthesis = checkParenthesis(expArr);
 
+    /*
     if (!testParenthesis) {
 
         console.log(errMsg + "vérifier les parenthèses.");
         return false;
 
-    }
+    }*/
 
-    console.log(expArr);
+    console.log(expArr.join(''));
 
 }
 
@@ -91,6 +150,6 @@ const findChars = (char, valueArr) => {
 
     return false;
 
-}
+} 
 
 argTester();
