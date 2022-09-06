f9c8160be308e7300c6ff7f27bc2aebed5636252
Dylan Tettarasar <dtettarasar@gmail.com>
2022-09-06 08:00:56 -0400

    feu02 : update in argTester func

--- feu02.js
+++ feu02.js
@@ -2,14 +2,27 @@
 
 const argTester = () => {
 
-    const errorMsg = "Veuillez passer les noms de deux fichiers en argument (Format lisible : .txt).\nExemple : node feu02.js board.txt to_find.txt";
+    const errorMsg = "Veuillez passer les noms de deux fichiers en argument (Format lisible : .txt).\nPassez en premier le plateau, en deuxième la forme à trouver. \nExemple : node feu02.js board.txt to_find.txt";
     const argument = process.argv.slice(2);
     const fileFormat = [".txt"];
 
+    let fileOnePath = null;
+    let fileTwoPath = null;
+
     if (argument.length !== 2) {
+
         console.log(errorMsg);
         return false;
+
+    } else {
+
+        fileOnePath = argument[0];
+        fileTwoPath = argument[1];
+
     }
+
+    console.log("fileOne: " + fileOnePath);
+    console.log("fileTwo: " + fileTwoPath);
 
 }
 
