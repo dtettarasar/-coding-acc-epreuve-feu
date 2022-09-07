c4729a1a5da47735a407e1678ebdf0c566598004
Dylan Tettarasar <dtettarasar@gmail.com>
2022-09-07 11:50:29 -0400

    feu02: created checkFileFormat func

--- feu02.js
+++ feu02.js
@@ -2,9 +2,8 @@
 
 const argTester = () => {
 
-    const errorMsg = "Veuillez passer les noms de deux fichiers en argument (Format lisible : .txt).\nPassez en premier le plateau, en deuxième la forme à trouver. \nExemple : node feu02.js board.txt to_find.txt";
+    const errorMsg = "Veuillez passer les noms de deux fichiers en argument (Format lisible : .txt, .md).\nPassez en premier le plateau, en deuxième la forme à trouver. \nExemple : node feu02.js board.txt to_find.txt";
     const argument = process.argv.slice(2);
-    const fileFormat = [".txt"];
 
     let fileOnePath = null;
     let fileTwoPath = null;
@@ -21,8 +20,30 @@ const argTester = () => {
 
     }
 
+    // Check les formats de fichiers 
+    if (!checkFileFormat(fileOnePath) || !checkFileFormat(fileTwoPath)) {
+        console.log(errorMsg);
+        return false;
+    }
+
     console.log("fileOne: " + fileOnePath);
     console.log("fileTwo: " + fileTwoPath);
+
+}
+
+const checkFileFormat = (filePath) => {
+
+    const fileFormat = [".txt", ".md"];
+
+    for (let i = 0; i < fileFormat.length; i++) {
+
+        if (filePath.endsWith(fileFormat[i])) {
+            return filePath;
+        }
+
+    }
+
+    return false;
 
 }
 
