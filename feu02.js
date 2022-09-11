943b898e813a517dc67c3b2724b493cb5f0d857f
Dylan Tettarasar <dtettarasar@gmail.com>
2022-09-11 17:24:11 +0200

    feu02 : created additional funcs

--- feu02.js
+++ feu02.js
@@ -59,6 +59,7 @@ const checkFileFormat = (filePath, formatArr) => {
 
 }
 
+// Récupérer les valeurs texte des fichiers
 const getTxtArr = (file) => {
 
     try {
@@ -75,14 +76,79 @@ const getTxtArr = (file) => {
 
 }
 
+// Comparer deux arrays
+const arrComparison = (arrOne, arrTwo) => {
+
+    if (arrOne.length === arrTwo.length) {
+
+        for (let i = 0; i < arrOne.length; i++) {
+
+            if (arrOne[i] !== arrTwo[i]) {
+                return false;
+            }
+
+        }
+
+        return true;
+    } 
+}
+
+// Vérifie si la deuxième string est bien présente dans la première
+const strInStr = (fullStr, sampleStr) => {
+
+    const fullStrArr = fullStr.split('');
+    const sampleStrArr = sampleStr.split('');
+    console.log("fullStrArr: " + fullStrArr);
+    console.log("sampleStrArr: " + sampleStrArr);
+
+    for (let i = 0; i < fullStrArr.length;i++) {
+
+        if (fullStrArr[i] === sampleStrArr[0]) {
+
+            const fullStrExtract = [];
+
+            for (let j = 0; j < sampleStrArr.length; j++) {
+
+                fullStrExtract.push(fullStrArr[i + j]);
+
+            }
+
+            const testComparison = arrComparison(fullStrExtract, sampleStrArr);
+
+            if (testComparison) {
+                return i;
+            } else {
+                return false;
+            }
+
+        }
+
+    }
+
+}
+
+const findValInBoard = (board, valToFind) => {
+
+    for (let i = 0; i < board.length; i++) {
+        console.log(board[i]);
+        console.log(strInStr(board[i], valToFind));
+    }
+
+}
+
 const main = () => {
 
     const argObj = argTester();
 
     if (argObj) {
         console.log(argObj);
+        //console.log(argObj.fileOneValue);
+        //console.log("fileTwo first line: " + argObj.fileTwoValue[0]);
+        //findValInBoard(argObj.fileOneValue, argObj.fileTwoValue[0]);
     }
 
 }
+
+console.log(strInStr("123456789\r", "123\r"));
 
 main();
