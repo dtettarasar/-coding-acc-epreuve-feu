78b49dfd558ae6bda5c28be3933793386cc2ed5e
Dylan Tettarasar <dtettarasar@gmail.com>
2022-10-04 07:06:02 -0400

    feu03 : update in argTester

--- feu03.js
+++ feu03.js
@@ -7,4 +7,23 @@ const argTester = () => {
     - vérifier que le fichier texte possède 9 lignes
     - avec une regex, vérifier que le texte de chaque ligne contient 9 char. chaque char est soit un nombre soit un point "."
     */
+
+    const argument = process.argv.slice(2);
+    const errorMsg = "erreur";
+    const fileFormats = [".txt", ".md"];
+    let boardFilePath = null;
+
+    if (argument.length !== 1) {
+
+        console.log(errorMsg);
+        return false;
+
+    } else {
+        boardFilePath = argument[0];
+    }
+
+    console.log(boardFilePath);
+
 }
+
+argTester();
