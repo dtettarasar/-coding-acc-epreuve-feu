6024024bef9fe5dd0cf9845da7e193907cc4d907
Dylan Tettarasar <dtettarasar@gmail.com>
2022-10-04 06:57:15 -0400

    feu03 : added requirment in argTester

--- feu03.js
+++ feu03.js
@@ -1,1 +1,10 @@
 // Sudoku
+
+const argTester = () => {
+    /*
+    - 1 seul fichier passé en argument
+    - fichier au format .txt
+    - vérifier que le fichier texte possède 9 lignes
+    - avec une regex, vérifier que le texte de chaque ligne contient 9 char. chaque char est soit un nombre soit un point "."
+    */
+}
