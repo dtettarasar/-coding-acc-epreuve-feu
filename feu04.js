abf3c15bbf2475c147a25e542d336dc6136cd58c
Dylan Tettarasar <dtettarasar@gmail.com>
2022-10-25 11:53:23 -0400

    feu04 : added getTxtArr func

--- feu04.js
+++ feu04.js
@@ -3,12 +3,13 @@
 const argTester = () => {
 
     const argument = process.argv.slice(2);
-    const errorMsg = "erreur";
+    const errorMsg = "Erreur: ";
     const fileFormats = [".txt", ".md"];
 
     if (argument.length !== 1) {
 
-        console.log(errorMsg);
+        console.log(errorMsg + "Veuillez passer le chemin du fichier en argument.");
+        console.log("Exemple: node feu04.js feu04_files/board1.txt");
         return false;
 
     } else {
@@ -19,11 +20,22 @@ const argTester = () => {
 
      // Check les formats de fichiers 
      if (!checkFileFormat(boardFilePath, fileFormats)) {
-        console.log(errorMsg);
+
+        console.log(errorMsg + "Vérifier le format du fichier. (Format(s) lisible(s) : " + fileFormats + ").");
         return false;
+
     }
 
-    console.log(boardFilePath);
+    const boardValue = getTxtArr(boardFilePath);
+
+    if (!boardValue) {
+
+        console.log("Erreur : le fichier n'existe pas (Vérifier le chemin du fichier).");
+        return false;
+
+    }
+
+    console.log(boardValue);
 
 }
 
@@ -32,12 +44,42 @@ const checkFileFormat = (filePath, formatArr) => {
     for (let i = 0; i < formatArr.length; i++) {
 
         if (filePath.endsWith(formatArr[i])) {
+
             return filePath;
+            
         }
 
     }
 
     return false;
+
+}
+
+// Récupérer les valeurs texte des fichiers
+const getTxtArr = (file) => {
+
+    try {
+
+        const txtArr = [];
+
+        const readline = require('readline');
+        const fs = require('fs');
+        const data = fs.readFileSync(file, 'utf8');
+
+        // split the contents by new line
+        const lines = data.split(/\r?\n/);
+
+        lines.forEach((line) => {
+            txtArr.push(line);
+        });
+
+        return txtArr;
+
+    } catch (error) {
+
+        return false;
+
+    }
 
 }
 
