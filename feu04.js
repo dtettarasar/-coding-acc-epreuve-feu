85d7fbc8e76c95b57f913101e59de5cd1c186c33
Dylan Tettarasar <dtettarasar@gmail.com>
2022-10-25 11:37:44 -0400

    feu04 : created argTester func

--- feu04.js
+++ feu04.js
@@ -1,1 +1,23 @@
 // Trouver le plus grand carré
+
+const argTester = () => {
+
+    const argument = process.argv.slice(2);
+    const errorMsg = "erreur";
+
+    if (argument.length !== 1) {
+
+        console.log(errorMsg);
+        return false;
+
+    } else {
+
+        boardFilePath = argument[0];
+
+    }
+
+    console.log(boardFilePath);
+
+}
+
+argTester();
